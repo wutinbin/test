dc756d2a1e8c6ee229ec1a38394c1f7a2a8ef1ea
wutinbing <1578463258@qq.com>
2021-01-27 00:49:29 +0800

    提交代码

---
+++ test-1-17/test-1-17/test.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+//
+//int num1 = 20;//全局变量
+//
+//int main()
+//{
+//	int a = 10;//局部变量
+//	printf("%d\n", a);
+//	return 0;
+//}
+#include <stdio.h>
+
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//	
+//	scanf("%d%d", &num1, &num2);
+//	
+//	sum = num1 + num2;
+//	printf("sum = %d\n", sum);
+//	return 0;
+//}
+
