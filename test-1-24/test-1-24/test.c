fa3e0853e4e0f1f94d9519bfc10edaa1416f2fed
wutinbing <1578463258@qq.com>
2021-01-29 19:15:53 +0800

    提交24号写的代码

---
+++ test-1-24/test-1-24/test.c
@@ -0,0 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//	scanf("%d%d", &num1, &num2);
+//	sum = num1 + num2;
+//	printf("sum = %d\n", sum);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arrl[] = "abc";//数组
+//	printf("%s\n", arrl);
+//	return 0;
+//}
+
+//int main()
+//{
+//	//printf("c:\\test\\32\\test.c");//\把后面的\注释掉了
+//	//printf("%c\n", '\'');
+//	printf("%s\n", "\"");
+//	return 0;
+
+//}
+
+//32选择语句
+//int main()
+//{
+//	int input = 0;
+//	printf("加入比特\n");
+//	printf("你要好好学习吗?(1/0)>: ");
+//	scanf("%d", &input);//1/0
+//	if (input == 1)
+//		printf("好offer\n");
+//	else
+//		printf("卖红薯\n");
+//	return 0;
+//}
+
+
+//46循环语句
+//int main()
+//{
+//	int line = 0;
+//	printf("加入比特\n");
+//
+//	while (line < 2000)
+//	{
+//		printf("敲一行代码:%d\n", line);
+//		line++;
+//	}
+//	if (line >= 2000);
+//	printf("好offer\n");
+//	return 0;
+//}
