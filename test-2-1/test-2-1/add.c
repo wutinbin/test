f8e22b992c2b5b8fe0857292f0d7877dfdef9772
wutinbing <1578463258@qq.com>
2021-02-01 22:17:11 +0800

    2-1测试代码

---
+++ test-2-1/test-2-1/add.c
@@ -0,0 +1,7 @@
+
+//定义一个函数
+//int Add(int x, int y)
+//{
+//	int z = x + y;
+//	return z;
+//}
---
+++ test-2-1/test-2-1/test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+//extern Add(int, int);//声明一个外部函数
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int sum = Add(a, b);
+//	printf("sum = %d\n", sum);
+//
+//	return 0;
+//}
+
+//指针
+//int main()
+//{
+//	int a = 10;//4个字节
+//	int* p = &a;//取地址  (有一种变量是用来存放地址的叫指针变量）
+//	//printf("%p\n", &a);
+//	//printf("%p\n", p);
+//	*p = 20;//* - 解引用操作符
+//	printf("a = %d\n", a);
+//	return 0;
+//}
