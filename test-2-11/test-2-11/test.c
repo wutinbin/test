cee9cc5fce71306199254df22e91ed3c7cc3a22e
wutinbing <1578463258@qq.com>
2021-02-14 15:35:27 +0800

    测试代码

---
+++ test-2-11/test-2-11/test.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+//int main()
+//{
+//	int day = 0;
+//	int n = 0;
+//	scanf_s("%d",&day);
+//	switch (day)
+//	{
+//	case 1:
+//		if (n == 1);//switch可以有if语句
+//		printf("hehe/n");
+//	case 2:
+//	case 3:
+//	case 4:
+//	case 5:
+//		printf("工作日\n");
+//		break;
+//	case 6:
+//	case 7:
+//		printf("休息日\n");
+//		break;
+//	default:
+//		printf("输入错误\n");
+//	//case 1://当day为1的情况
+//	//	printf("星期一\n");
+//	//case 2:
+//	//	printf("星期二\n");
+//	//	break;//终止的意思
+//	//case 3:
+//	//	printf("星期三\n");
+//	}
+//	return 0;
+//}
+
+//打印1到10
+//int main()
+//{
+//	int i = 1;
+//	while (i<=10)
+//	{
+//		printf("%d ", i);
+//		i++;
+//	}
+//	return 0;
+//}
