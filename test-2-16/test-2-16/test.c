09428ca5ff0c88c88dbe8ff89d1189915e8650e0
wutinbing <1578463258@qq.com>
2021-02-17 18:37:31 +0800

    2.16测试代码

---
+++ test-2-16/test-2-16/test.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch = getchar()) != EOF)
+//	{
+//		if (ch<'0' || ch>'9')
+//			continue;
+//		putchar(ch);
+//	}
+	//return 0;
+//}
+
+
+//用for循环打印1到10
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i<=10; i++)
+//	{
+//		printf("%d", i);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	for (;;)//省略判断部分，则表示恒为真
+//	{
+//		printf("haha\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d", arr[i]);
+//	}
+//	return 0;
+//}
