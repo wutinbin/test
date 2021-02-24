f2c9e94eafe33bd99fb37f496e7be1822e78825d
wutinbing <1578463258@qq.com>
2021-02-24 23:41:18 +0800

    2-24测试代码

---
+++ test-2-24/test.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		int j = 0;
+//		for (j = 2; j < i; j++)
+//		{
+//			if (i%j == 0)
+//			{
+//				break;
+//			}
+//		}
+//		if (j == i)
+//		{
+//			count++;
+//			printf("%d", i);
+//		}
+//	}
+//	printf("\ncount=%d\n", count);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0, b = 0;
+//	for (a = 1, b = 1; a <= 100; a++)
+//	{
+//		if (b >= 20)break;
+//		if (b % 3 == 1)
+//		{
+//			b = b + 3;
+//			continue;
+//		}
+//		b = b + 5;
+//	}
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+//编写程序输出1到100所有整数中出现多少个数字9
+
+int main()
+{
+	int i = 0;
+	int count = 0;
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 10 == 9)
+			count++;
+		if (i / 10 == 9)
+			count++;
+	}
+	printf("count=%d\n", count);
+	return 0;
+}
