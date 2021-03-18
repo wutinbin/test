e97976a84ba6f4f8716293c2684e76bffa686f55
wutinbing <1578463258@qq.com>
2021-03-18 17:28:06 +0800

    3-18测试代码

---
+++ test-3-18/test.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+
+//指针数组
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int c = 30;
+	int* arr[3] = { &a, &b, &c };
+	int i = 0;
+	for (i = 0; i < 3; i++)
+	{
+		printf("%d ", *(arr[i]));
+	}
+	return 0;
+}
+
+
+//数组可以通过指针来访问
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int* p = arr;
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		*(p + i) = i;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", *(p + i));
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//	printf("%d\n", &arr[9] - &arr[0]);//指针减去指针对于中间元素的个数
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int* p = arr;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", *p);
+//		p = p + 1;
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int* p = arr;//数组名-首元素的地址
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		*(p + i) = 1;
+//	}
+//	return 0;
+//}
